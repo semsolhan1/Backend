66cc8517330e2241597284aacdeaf71eaccc5b9f
flatcalm <a_abnc@naver.com>
2023-07-03 18:25:10 +0900

    User Entity, Controller 작성 중...

--- src/main/java/com/handifarm/api/user/controller/UserController.java
+++ src/main/java/com/handifarm/api/user/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.handifarm.api.user.controller;
 
+import com.handifarm.api.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
+
+    private final UserService service;
+
 }
--- src/main/java/com/handifarm/api/user/entity/User.java
+++ src/main/java/com/handifarm/api/user/entity/User.java
@@ -1,4 +1,49 @@
 package com.handifarm.api.user.entity;
 
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Setter @Getter
+@ToString @EqualsAndHashCode()
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_users")
 public class User {
+
+    @Id
+    private String userId;
+
+    @Column(nullable = false)
+    private String userPw;
+
+    @Column(nullable = false)
+    private String userName;
+
+    private String userNick;
+
+    @Column(nullable = false)
+    private String userPhone;
+
+    @Column(nullable = false)
+    private String userEmail1;
+    @Column(nullable = false)
+    private String userEmail2;
+
+    @Column(nullable = false)
+    private String addrBasic;
+    @Column(nullable = false)
+    private String addrDetail;
+    @Column(nullable = false)
+    private int addrZipNum;
+
+    @CreationTimestamp
+    private LocalDateTime regDate;
+
 }
