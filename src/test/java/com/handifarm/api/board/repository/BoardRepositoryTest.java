070ec275c3cb5773e6244d208f12bd9bee70f229
1roo <93hanaru@gmail.com>
2023-07-12 20:51:12 +0900

    230712 board 구현

---
+++ src/test/java/com/handifarm/api/board/repository/BoardRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.handifarm.api.board.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class BoardRepositoryTest {
+
+}
