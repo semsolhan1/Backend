3d134f7f09a4da86145eb7b5fb171e8d049154e3
flatcalm <a_abnc@naver.com>
2023-08-03 12:44:31 +0900

    230803 - 오전 작업 내용

--- src/main/java/com/handifarm/api/snsBoard/dto/request/SnsBoardCreateRequestDTO.java
+++ src/main/java/com/handifarm/api/snsBoard/dto/request/SnsBoardCreateRequestDTO.java
@@ -1,6 +1,7 @@
 package com.handifarm.api.snsBoard.dto.request;
 
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
 import java.util.List;
@@ -15,5 +16,7 @@ public class SnsBoardCreateRequestDTO {
     private String content;
 
     private List<String> hashTags;
+
+    private List<MultipartFile> snsImgs;
 
 }
